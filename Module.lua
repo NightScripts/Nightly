0d0be1cadf0a84cc6edf7ed2de0978395e3f0835
Night <166496112+NightScripts@users.noreply.github.com>
2024-07-16 01:16:43 +0800

    Create Module.lua
    Module - Nightly Menu

---
+++ Module.lua
@@ -0,0 +1,36 @@
+local Nightly = {}
+
+function retjs(url)
+    local resp = game:HttpGet(url)
+    local https = game:GetService("HttpService")
+    return https:JSONDecode(resp)
+end
+
+function retpurl(placeId, gd)
+    return gd[tostring(placeId)]
+end
+
+function exeps(url)
+    if url then
+        local sc = game:HttpGet(url)
+        loadstring(sc)()
+    else
+        error("Invalid Place ID or URL not found")
+    end
+end
+
+function Nightly.load()
+    local jsurl = "https://raw.githubusercontent.com/NightScripts/Nightly/main/Games.json"
+    local gd = retjs(jsurl)
+    local placeId = game.PlaceId
+    local psu = retpurl(placeId, gd)
+    exeps(psu)
+end
+
+function Nightly.get()
+    local executor = identifyexecutor()
+    local pi = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
+    return executor .. " Nightly Loaded" .. pi.Name
+end
+
+return Nightly
