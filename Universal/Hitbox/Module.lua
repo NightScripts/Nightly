1c954433c0a73c90a88c04566de055110d9e2ae9
Night <166496112+NightScripts@users.noreply.github.com>
2024-07-19 21:27:45 +0800

    Create Module.lua
    Module for Hitbox Func

---
+++ Universal/Hitbox/Module.lua
@@ -0,0 +1,13 @@
+-- NightHit.lua
+local NightHit = {}
+
+NightHit.Config = {
+    BoxActive = true,
+    TeamCheck = false,
+    BoxSize = 5,
+    BoxTransparency = 0.5,
+    BoxColor = "Pink",
+    BoxMaterial = "Glass",
+}
+
+return NightHit
